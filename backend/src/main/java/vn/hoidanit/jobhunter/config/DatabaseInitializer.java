50d9e055df7ced86129b1568e4fe43f077e7ef77
duylam_dev <lam2012003az@gmail.com>
2025-04-03 00:21:15 +0700

    +

--- backend/src/main/java/vn/hoidanit/jobhunter/config/DatabaseInitializer.java
+++ backend/src/main/java/vn/hoidanit/jobhunter/config/DatabaseInitializer.java
@@ -76,6 +76,7 @@ public class DatabaseInitializer implements CommandLineRunner {
 
             arr.add(new Permission("Download a file", "/api/v1/files", "POST", "FILES"));
             arr.add(new Permission("Upload a file", "/api/v1/files", "GET", "FILES"));
+            arr.add(new Permission("fetch account", "/auth/account", "GET", "AUTH"));
 
             this.permissionRepository.saveAll(arr);
         }
